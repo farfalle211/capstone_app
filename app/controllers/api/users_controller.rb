53208d5651bf0ba59f8f71fbf262a652c6f782b1
farfalle211 <farfalle211@gmail.com>
2019-03-27 09:38:32 -0500

    minor change to users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -7,7 +7,7 @@ class Api::UsersController < ApplicationController
 
   def create
     user = User.new(
-      name: params[:name],
+      name: params[:name].titleize,
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -31,7 +31,7 @@ class UserEvent < ApplicationRecord
     p eLat
     p eLon
 
-    Geocoder::Calculations.distance_between([uLat, uLon], [eLat, eLon]) < 40
+    Geocoder::Calculations.distance_between([uLat, uLon], [eLat, eLon]) < 5
   end
 
 end
