afa5b91533635d50d2e7995a142bf425c0f7a02b
farfalle211 <farfalle211@gmail.com>
2019-03-27 11:40:37 -0500

    added a dependent destroy for requests

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -5,7 +5,7 @@ class UserEvent < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  has_many :requests
+  has_many :requests, dependent: :destroy
   has_many :groups, through: :requests
 
   enum confirmation_status: {not_purchased: 1, purchased: 2, attended: 3}
