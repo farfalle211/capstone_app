21a659716d73107af248b1c32a2cdfa9cc78e04a
farfalle211 <farfalle211@gmail.com>
2019-04-04 11:30:52 -0500

    added vue frontend

--- config/routes.rb
+++ config/routes.rb
@@ -38,4 +38,6 @@ Rails.application.routes.draw do
     delete '/groups/:id' => 'groups#destroy'
 
   end
+
+  get "/*path" => proc { [200, {}, [ActionView::Base.new.render(file: 'public/index.html')]] }
 end
