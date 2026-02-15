59bf0ed4fd13e095fa04f03460eabb11be997c17
Mike Dessailly <mike.dessailly@datadoghq.com>
2026-02-15 15:52:11 -0500

    fix sast security issues

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,17 +2,21 @@ class Api::SessionsController < ApplicationController
   def create
     email    = params[:email]    || params.dig(:session, :email)    || params.dig(:api_session, :email)
     password = params[:password] || params.dig(:session, :password) || params.dig(:api_session, :password)
+    email = email.to_s.strip.presence
+    return render json: {}, status: :unauthorized if email.blank? || password.blank?
+
     user = User.find_by(email: email)
-    if user && user.authenticate(password)
+    if user&.authenticate(password)
       jwt = JWT.encode(
         {
-          user_id: user.id, # the data to encode
-          exp: 24.hours.from_now.to_i # the expiration time
+          user_id: user.id,
+          exp: 24.hours.from_now.to_i
         },
-        Rails.application.credentials.fetch(:secret_key_base), # the secret key
-        'HS256' # the encryption algorithm
+        Rails.application.credentials.fetch(:secret_key_base),
+        'HS256'
       )
-      render json: {jwt: jwt, email: user.email, user_id: user.id}, status: :created
+      # Only expose whitelisted attributes; use DB values to avoid path/injection from params
+      render json: { jwt: jwt, email: user.email.to_s, user_id: user.id }, status: :created
     else
       render json: {}, status: :unauthorized
     end
